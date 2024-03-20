bb59c9b68ba5e8bf8479e103da9a613bb55b0f85
leobewater <mmbee888@gmail.com>
2024-03-20 08:08:24 -0400

    copy news and auth files

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { enhance } from '$app/forms';
+	import { page } from '$app/stores';
+	export let form;
+	const redirectTo = $page.url.searchParams.get('redirectTo') || '/';
+</script>
+
+<form method="post" action="?/login&redirectTo={redirectTo}" use:enhance>
+	<p>{form?.message || ''}</p>
+	<input type="text" name="username" placeholder="Username" value={form?.username ?? ''} />
+	<input type="password" name="password" placeholder="Password" />
+	<button>Login</button>
+	<button formaction="?/register&redirectTo={redirectTo}">Register</button>
+</form>
---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Today's headlines</h1>
+{#each data.news as article}
+	<h2>{article.title}</h2>
+{/each}
